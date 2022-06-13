a2511aef7a82f3ea272d404621dccad20d86cf02
Viktor <suetnovvic@yandex.com>
2022-06-13 11:57:25 +0300

    added constructor and getters

--- src/main/java/com/viktorsuetnov/chat/Message.java
+++ src/main/java/com/viktorsuetnov/chat/Message.java
@@ -4,6 +4,19 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
 
-    private MessageType messageType;
-    private String data;
+    private final MessageType messageType;
+    private final String data;
+
+    public Message(MessageType messageType, String data) {
+        this.messageType = messageType;
+        this.data = data;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
