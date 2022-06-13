231b3de7e9c537ac516a28dba66587f16689e0cb
Viktor <suetnovvic@yandex.com>
2022-06-13 11:47:48 +0300

    added User class with field username

---
+++ src/main/java/com/viktorsuetnov/chat/User.java
@@ -0,0 +1,40 @@
+package com.viktorsuetnov.chat;
+
+import java.util.Objects;
+
+public class User {
+
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
